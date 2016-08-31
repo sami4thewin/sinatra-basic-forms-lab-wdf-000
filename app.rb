e53fd19f3dbc872abddc103b73bd1a6b22875cd3
sami4thewin <samiomarmohammed@gmail.com>
2016-08-31 01:28:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    bruh = params[:age]
+    ayo = bruh.slice(/[\d]*/)
+    ayo = ayo.to_i
+    @puppy = Puppy.new(params[:name], params[:breed], ayo)
+    # binding.pry
+    erb :display_puppy
+  end
+
 end
